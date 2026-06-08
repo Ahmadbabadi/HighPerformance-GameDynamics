1f72454e70b7ae2d6b6f6e301d4ad42b0403ea40
Ahmad Babadi <ahmad.babadi.96@gmail.com>
2026-06-08 21:06:55 +0200

    Allocations was refined in this version to see the difference of runingtime.

---
+++ src/pgg_julia_opt.jl
@@ -0,0 +1,158 @@
+import StatsBase as sb
+import Statistics
+using Random
+import BenchmarkTools
+
+
+function build_units(lattice_size)
+    L = Int32(lattice_size) #  lattice_size <= 46,340$
+    units = zeros(Int32, L^2, 5)
+    for i = axes(units, 1)
+        x = mod1(i, L)
+        y = cld(i, L)
+        units[i, 1] = i
+        units[i, 2] = (y - 1) * L + mod1(x - 1, L)
+        units[i, 3] = (y - 1) * L + mod1(x + 1, L)
+        units[i, 4] = (mod1(y - 1, L) - 1) * L + x
+        units[i, 5] =(mod1(y + 1, L) - 1) * L + x
+    end
+    return units
+end
+
+
+function pgg_init(lattice_size, memory_length, cooperator_rate=1/3, defector_rate=1/3)
+    units = build_units(lattice_size)
+    item_weights = sb.Weights([cooperator_rate, defector_rate, 1-(cooperator_rate+defector_rate)])
+    strategies = sb.sample(Int8[1, 2, 3], item_weights, lattice_size^2) # use [1, 2, 3] because is compatible to julia index
+    memories = ones(Float32, lattice_size^2, 3, memory_length)
+    return units, strategies, memories
+end
+
+
+
+function payoff_calculator(unit, strategies, synergy_rate, cost)
+    coopator_payoff = cost
+    defector_payoff = cost
+    loner_payoff    = cost
+    cooprators = 0
+    participants = 0
+    for neigbor = unit
+        S = strategies[neigbor]
+        if S == 1
+            cooprators += 1
+            participants += 1
+        elseif S == 2
+            participants += 1
+        end
+    end
+
+    if participants > 1
+        coopator_payoff = cost*synergy_rate*cooprators/participants
+        defector_payoff = coopator_payoff + cost
+    end
+    
+    return coopator_payoff, defector_payoff, loner_payoff
+
+end
+
+function build_cumulative_payoffs(strategies, cumulative_payoffs, units, synergy_rate, cost)
+    fill!(cumulative_payoffs, 0.0)
+    for p = eachindex(strategies)
+        unit = @view units[p, :]
+        c, d, l = payoff_calculator(unit, strategies, synergy_rate, cost)
+        for pp = unit
+            s = strategies[pp]
+            v = s == 1 ? c : s == 2 ? d : l
+            cumulative_payoffs[pp] += v
+        end
+    end
+
+    return cumulative_payoffs
+end
+
+
+function update_memory(memories, strategies, cumulative_payoffs, memory_pointer, buffer_matrix)
+    sum!(buffer_matrix, memories)
+    memory_length = size(memories, 3)
+    buffer_matrix ./= memory_length
+    for i in 1:length(strategies)
+        buffer_matrix[i, strategies[i]] = cumulative_payoffs[i]
+    end
+    memories[:, :, memory_pointer] .= buffer_matrix
+    return memories, mod1(memory_pointer + 1, memory_length)
+end
+
+
+function pick_new_strategy(strategies, memories, units, beta)
+    new_strategies = deepcopy(strategies)
+    for p = eachindex(strategies)
+        selected_neighbor = units[p, rand(2:5)]
+        if strategies[p] != strategies[selected_neighbor]
+            memory_player = Statistics.mean(view( memories, p, strategies[p], :))
+            memory_neighbor = Statistics.mean(view( memories, selected_neighbor, strategies[selected_neighbor], :))
+            weight = 1/(1+exp(beta*(memory_player-memory_neighbor)))
+            if rand() < weight
+                new_strategies[p] = strategies[selected_neighbor]
+            end
+        end
+    end
+    return new_strategies
+end
+
+
+function useful_data(strategies)
+    N = length(strategies)
+    cooperators = count(==(1), strategies)
+    defectors = count(==(2), strategies)
+    return [cooperators / N, defectors / N]
+end
+
+
+function pgg_main(lattice_size, memory_length, synergy_rate, rounds, beta=10, cost=1/5, cooperator_rate=1/3, defector_rate=1/3)
+    N = lattice_size^2
+    data = zeros(rounds+1, 2)
+    memory_pointer = 1
+    units, strategies, memories = pgg_init(lattice_size, memory_length, cooperator_rate, defector_rate)
+    data[1, :] = useful_data(strategies)
+    cumulative_payoffs = zeros(Float32, N)
+    buffer_matrix = zeros(Float32, N, 3)
+    for r in 2:rounds+1
+        cumulative_payoffs = build_cumulative_payoffs(strategies, cumulative_payoffs, units, synergy_rate, cost)
+        memories, memory_pointer = update_memory(memories, strategies, cumulative_payoffs, memory_pointer, buffer_matrix)
+        strategies = pick_new_strategy(strategies, memories, units, beta)
+        data[r, :] = useful_data(strategies)
+    end
+    return data
+end
+
+
+# Random.seed!(1234)
+
+lattice_size = 4096
+lattice_size = 512
+memory_length = 5
+synergy_rate = Float32(3.6)
+cost = Float32(1/5)
+beta = Int32(10)
+units, strategies, memories = pgg_init(lattice_size, memory_length)
+memory_pointer = 1
+cumulative_payoffs = zeros(Float32, length(strategies))
+cumulative_payoffs = build_cumulative_payoffs(strategies, cumulative_payoffs, units, synergy_rate, cost)
+buffer_matrix = zeros(Float32, lattice_size^2, 3)
+buffer_count = falses(length(strategies))
+memories, memory_pointer = update_memory(memories, strategies, cumulative_payoffs, memory_pointer, buffer_matrix)
+# BenchmarkTools.@btime build_units($lattice_size)
+# BenchmarkTools.@btime pgg_init(lattice_size, memory_length)
+# BenchmarkTools.@btime build_cumulative_payoffs($strategies, cumulative_payoffs, $units, $synergy_rate, $cost)
+# BenchmarkTools.@btime update_memory($memories, $strategies, $cumulative_payoffs, 1)
+# BenchmarkTools.@btime useful_data($strategies)
+
+# BenchmarkTools.@time pgg_main(256, 5, 3.5, 1000)
+# BenchmarkTools.@time pgg_main(512, 5, 3.5, 1000)
+# BenchmarkTools.@time pgg_main(1024, 5, 3.5, 1000)
+# BenchmarkTools.@time pgg_main(2048, 1, 4.5, 1000)
+# BenchmarkTools.@btime pgg_main(2048, 5, 3.5, 1000)
+println()
+
+# data = pgg_main(1024, 1, 4.5, 1000)
+# print(data[990:1001])
