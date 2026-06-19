59c92a342c26eb5331ccc49a21846cd704b5312c
Ahmad Babadi <ahmad.babadi.96@gmail.com>
2026-06-19 10:24:34 +0200

    remove testing lines

--- src/pgg_numba.py
+++ src/pgg_numba.py
@@ -105,62 +105,40 @@ def _useful_data(state):
     return np.array([cooperators_rate, defectors_rate])
 
 
-def init_simulation(
-                    lattice_size,
-                    memory_length,
-                    synergy_rate,
-                    cost=1 / 5,
-                    beta=10,
-                    cooperators_rate=1 / 3,
-                    defectors_rate=1 / 3,
-                    ):
+def init_simulation(lattice_size, memory_length, synergy_rate, cost=1 / 5, beta=10, cooperators_rate=1 / 3, defectors_rate=1 / 3):
 
     N = lattice_size ** 2
     strategies = np.random.choice([0, 1, 2], size=N,
                                   p=[defectors_rate, cooperators_rate, 1 - (cooperators_rate + defectors_rate)],
                                   ).astype(np.int64)
 
-    state = dict(
-        lattice_size    = lattice_size,
-        memory_length   = memory_length,
-        synergy_rate    = synergy_rate,
-        cost            = cost,
-        beta            = beta,
-        strategies      = strategies,
-        neighbors       = _build_neighbors(lattice_size),
-        memories        = np.ones((N, 3, memory_length), dtype=np.float32),
-        memory_pointer  = 0,
-        initial_features = np.array(
-            [lattice_size, memory_length, synergy_rate, beta,
-             cooperators_rate, defectors_rate]
-        ),
-    )
+    state = dict(lattice_size    = lattice_size,
+                 memory_length   = memory_length,
+                 synergy_rate    = synergy_rate,
+                 cost            = cost,
+                 beta            = beta,
+                 strategies      = strategies,
+                 neighbors       = _build_neighbors(lattice_size),
+                 memories        = np.ones((N, 3, memory_length), dtype=np.float32),
+                 memory_pointer  = 0,
+                 initial_features = np.array([lattice_size, memory_length, synergy_rate, beta,
+                                              cooperators_rate, defectors_rate])
+                )
     return state
 
 
 def get_payoff_and_update_memory(state):
-    cumulative_payoff = _build_cumulative_payoffs(
-        state["strategies"],
-        state["synergy_rate"],
-        state["cost"],
-        state["neighbors"],
-    )
-    _update_memory(
-        state["memories"],
-        state["strategies"],
-        cumulative_payoff,
-        state["memory_pointer"],
-    )
+    cumulative_payoff = _build_cumulative_payoffs(state["strategies"], state["synergy_rate"],
+                                                  state["cost"], state["neighbors"])
+    _update_memory(state["memories"], state["strategies"],
+                   cumulative_payoff, state["memory_pointer"])
     state["memory_pointer"] = (state["memory_pointer"] + 1) % state["memory_length"]
     return state
 
 
 def pick_new_strategy(state):
-    state["strategies"] = _pick_new_strategy(state["strategies"],
-                                             state["memories"],
-                                             state["beta"],
-                                             state["neighbors"]
-                                             )
+    state["strategies"] = _pick_new_strategy(state["strategies"], state["memories"],
+                                             state["beta"], state["neighbors"])
     return state
 
 
@@ -177,24 +155,6 @@ def run_simulation(state, rounds):
 
 def save_simulation(state):
     f = state["initial_features"]
-    np.savez_compressed(
-                        "pgg_{}_{}_{}.npz".format(int(f[0]), int(f[1]), f[2]),
-                        initial_features=f,
-                        data=state["data"],
-                        )
+    np.savez_compressed("pgg_{}_{}_{}.npz".format(int(f[0]), int(f[1]), f[2]),
+                        initial_features=f, data=state["data"])
 
-
-if __name__ == "__main__":
-    state = init_simulation(
-                            lattice_size    = 512,
-                            memory_length   = 1,
-                            synergy_rate    = 4.5,
-                            cost            = 1 / 5,
-                            beta            = 10,
-                            cooperators_rate = 1 / 3,
-                            defectors_rate   = 1 / 3,
-                            )
-    start = time.perf_counter()
-    state = run_simulation(state, rounds=1000)
-    print(time.perf_counter() - start)
-    print(state["data"][-1])
