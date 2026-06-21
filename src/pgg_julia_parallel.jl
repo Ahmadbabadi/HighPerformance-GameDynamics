eab8ca7700b3d645cf33865cada8fcdd02417735
Ahmad Babadi <ahmad.babadi.96@gmail.com>
2026-06-21 19:47:27 +0200

    perf: rewrite parallel PGG simulation for correct and efficient threading
    - Fix race condition in build_cumulative_payoffs by computing the all units payoffs of each player. It makes totally seprate processes.
    
    - Reorder memories array from [i, s, t] to [t, s, i]
    
    - Remove threading in useful_data. It was overhead for this reduction
    
    - using "strategies .= new_strategies" instead of Copy()
    
    - Remove @threads from update_memory

--- src/pgg_julia_parallel.jl
+++ src/pgg_julia_parallel.jl
@@ -1,7 +1,6 @@
 import StatsBase as sb
 import Statistics
 using Random
-import BenchmarkTools
 using Base.Threads
 
 
@@ -24,23 +23,22 @@ end
 function pgg_init(lattice_size, memory_length, cooperator_rate=1/3, defector_rate=1/3)
     units = build_units(lattice_size)
     item_weights = sb.Weights([cooperator_rate, defector_rate, 1-(cooperator_rate+defector_rate)])
-    strategies = sb.sample(Int8[1, 2, 3], item_weights, lattice_size^2) # use [1, 2, 3] because is compatible to julia index
-    memories = ones(Float32, lattice_size^2, 3, memory_length)
+    strategies = sb.sample(Int8[1, 2, 3], item_weights, lattice_size^2)
+    memories = ones(Float32, memory_length, 3, lattice_size^2)  # [t, s, i]
     return units, strategies, memories
 end
 
 
-
 function payoff_calculator(unit, strategies, synergy_rate, cost)
-    coopator_payoff = cost
+    cooperator_payoff = cost
     defector_payoff = cost
     loner_payoff = cost
-    cooprators = 0
+    cooperators  = 0
     participants = 0
-    for neigbor = unit
-        S = strategies[neigbor]
+    for neighbor in unit
+        S = strategies[neighbor]
         if S == 1
-            cooprators += 1
+            cooperators  += 1
             participants += 1
         elseif S == 2
             participants += 1
@@ -48,68 +46,57 @@ function payoff_calculator(unit, strategies, synergy_rate, cost)
     end
 
     if participants > 1
-        coopator_payoff = cost*synergy_rate*cooprators/participants
-        defector_payoff = coopator_payoff + cost
+        cooperator_payoff = cost * synergy_rate * cooperators / participants
+        defector_payoff   = cooperator_payoff + cost
     end
-    
-    return coopator_payoff, defector_payoff, loner_payoff
 
+    return cooperator_payoff, defector_payoff, loner_payoff
 end
+
 
 function build_cumulative_payoffs(strategies, cumulative_payoffs, units, synergy_rate, cost)
     fill!(cumulative_payoffs, 0.0)
-    for p = eachindex(strategies)
-        unit = @view units[p, :]
-        c, d, l = payoff_calculator(unit, strategies, synergy_rate, cost)
-        for pp = unit
-            s = strategies[pp]
-            v = s == 1 ? c : s == 2 ? d : l
-            cumulative_payoffs[pp] += v
+
+    @threads :static for p in eachindex(strategies)
+        total = 0.0f0
+        s_p = strategies[p]
+        for g in @view units[p, :]
+            unit_g = @view units[g, :]
+            c, d, l = payoff_calculator(unit_g, strategies, synergy_rate, cost)
+            total += s_p == 1 ? c : s_p == 2 ? d : l
         end
+        cumulative_payoffs[p] = total
     end
 
     return cumulative_payoffs
 end
 
 
-function update_memory(memories, strategies, cumulative_payoffs, memory_pointer, buffer_matrix)
-    sum!(buffer_matrix, memories)
-    memory_length = size(memories,3)
-    buffer_matrix ./= memory_length
-
-    Threads.@threads for i in eachindex(strategies)
-        buffer_matrix[i, strategies[i]] = cumulative_payoffs[i]
+function update_memory(memories, strategies, cumulative_payoffs, memory_pointer)
+    memory_length = size(memories, 1)
+    for i in eachindex(strategies)
+        s = strategies[i]
+        memories[memory_pointer, s, i] = cumulative_payoffs[i]
     end
-
-    memories[:,:,memory_pointer] .= buffer_matrix
-    return memories, mod1(memory_pointer+1,memory_length)
+    return memories, mod1(memory_pointer + 1, memory_length)
 end
 
 
-
-
-function pick_new_strategy(strategies, memories, units, beta)
-    new_strategies = similar(strategies)
-    @threads for p in eachindex(strategies)
+function pick_new_strategy!(new_strategies, strategies, memories, units, beta)
+    @threads :static for p in eachindex(strategies)
         selected_neighbor = units[p, rand(2:5)]
         s_p = strategies[p]
         s_n = strategies[selected_neighbor]
 
         if s_p != s_n
-            memory_player = Statistics.mean(view(memories, p, s_p, :))
-            memory_neighbor = Statistics.mean(view(memories, selected_neighbor, s_n, :))
-
-            weight = 1/(1+exp(beta*(memory_player-memory_neighbor)))
-            if rand() < weight
-                new_strategies[p] = s_n
-            else
-                new_strategies[p] = s_p
-            end
+            memory_player = Statistics.mean(view(memories, :, s_p, p))
+            memory_neighbor = Statistics.mean(view(memories, :, s_n, selected_neighbor))
+            weight = 1 / (1 + exp(beta * (memory_player - memory_neighbor)))
+            new_strategies[p] = rand() < weight ? s_n : s_p
         else
             new_strategies[p] = s_p
         end
     end
-
     return new_strategies
 end
 
@@ -117,37 +104,28 @@ end
 function useful_data(strategies)
     N = length(strategies)
     cooperators = count(==(1), strategies)
-    defectors = count(==(2), strategies)
+    defectors   = count(==(2), strategies)
     return [cooperators / N, defectors / N]
 end
 
-function useful_data_parallel(strategies)
-    N = length(strategies)
-    cooperators = 0
-    defectors = 0
-    @threads for s in strategies
-        if s==1
-            cooperators += 1
-        elseif s==2
-            defectors += 1
-        end
-    end
-    return [cooperators/N, defectors/N]
-end
 
-
-function run_simulation(lattice_size, memory_length, synergy_rate, rounds, beta=10, cost=1/5, cooperator_rate=1/3, defector_rate=1/3)
+function run_simulation(lattice_size, memory_length, synergy_rate, rounds,
+                        beta=10, cost=1/5, cooperator_rate=1/3, defector_rate=1/3)
     N = lattice_size^2
-    data = zeros(rounds+1, 2)
+    data = zeros(rounds + 1, 2)
     memory_pointer = 1
+
     units, strategies, memories = pgg_init(lattice_size, memory_length, cooperator_rate, defector_rate)
-    data[1, :] = useful_data(strategies)
     cumulative_payoffs = zeros(Float32, N)
-    memory_means = deepcopy(memories[:, :, 1])
+    new_strategies     = similar(strategies)
+
+    data[1, :] = useful_data(strategies)
+
     for r in 2:rounds+1
-        cumulative_payoffs = build_cumulative_payoffs(strategies, cumulative_payoffs, units, synergy_rate, cost)
-        memories, memory_pointer = update_memory(memories, strategies, cumulative_payoffs, memory_pointer, memory_means)
-        strategies = pick_new_strategy(strategies, memory_means, units, beta)
+        build_cumulative_payoffs(strategies, cumulative_payoffs, units, synergy_rate, cost)
+        memories, memory_pointer = update_memory(memories, strategies, cumulative_payoffs, memory_pointer)
+        pick_new_strategy!(new_strategies, strategies, memories, units, beta)
+        strategies .= new_strategies
         data[r, :] = useful_data(strategies)
     end
     return data
